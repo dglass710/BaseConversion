012226561cfae245b75bf0a8bd0e3ed92a65dff6
dglass710 <66642674+dglass710@users.noreply.github.com>
2020-09-10 22:34:42 -0500

    Update and rename binToTen to binToTen.py

--- binToTen.py
+++ binToTen.py
@@ -1,7 +1,3 @@
-#! /Library/Frameworks/Python.framework/Versions/3.8/bin/python3
-
-import sys
-
 def main(a):
     'Takes a binary and returns an int of equal value'
     for char in str(a):
@@ -13,4 +9,3 @@ def main(a):
         aBaseTen += 2 ** power * int(digit)
         power += 1
     return aBaseTen 
-print(main(sys.argv[1]))
